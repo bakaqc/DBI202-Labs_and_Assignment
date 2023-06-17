d3b6bbe7dae2796c94b70ddbfa832ed461665c9e
Minh Vương <hardingadonis@gmail.com>
2023-06-17 23:06:19 +0700

    Upload ``Lab4/Q5.sql`` and ``Lab4/Q6.sql``

---
+++ Labs/Lab4/Q5.sql
@@ -0,0 +1,15 @@
+USE FUH_COMPANY
+GO
+
+-- Step 1: Declare a variable.
+Declare @SupervisorSSN VARCHAR(50);
+
+-- Step 2: Find supervisorSSN of staff named Mai Duy An and assign to @SupervisorSSN.
+SELECT @SupervisorSSN = supervisorSSN
+FROM tblEmployee
+WHERE empName = N'Mai Duy An'
+
+-- Step 3: Find empSSN and empName by @SupervisorSSN.
+SELECT empSSN AS 'Employee SSN', empName AS 'Employee Name'
+FROM tblEmployee
+WHERE empSSN = @SupervisorSSN;
