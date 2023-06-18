9029be4797b4ae963157fe7272ce72589aa30565
Đinh Quốc CHương <quocchuong2609@gmail.com>
2023-06-18 16:38:36 +0700

    Upload Q3 & Q4

---
+++ Labs/Lab4/Q4.sql
@@ -0,0 +1,17 @@
+﻿USE FUH_COMPANY
+
+GO 
+
+/*
+Show bảng Employee
+
+SELECT *
+FROM tblEmployee
+*/
+
+/* Đáp án Câu 4 */
+SELECT empSSN, empName
+FROM tblEmployee
+WHERE supervisorSSN = (SELECT empSSN
+					   FROM tblEmployee
+					   WHERE empName = 'Mai Duy An');
