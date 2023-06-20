23c48a2a87264666caa6a2b3df9846876fee54da
yuhtnguyen <nguyenthithuy1022003@gmail.com>
2023-06-20 11:27:52 +0700

    Submit

---
+++ Labs/Lab4/Q1.sql
@@ -0,0 +1,19 @@
+﻿
+/*
+1. 
+- Please indicate who is managing the department named: Research and Development Department. 
+- Required information: code, employee name, department number, department name 
+*/
+
+USE FUH_COMPANY
+GO 
+
+SELECT *
+FROM tblDepartment; 
+
+SELECT *
+FROM tblEmployee; 
+
+SELECT empSSN,empName,d.depNum, depName
+FROM tblDepartment d JOIN tblEmployee e ON d.mgrSSN = e.empSSN
+WHERE depName = N'Phòng Nghiên cứu và phát triển'
---
+++ Labs/Lab4/Q2.sql
@@ -0,0 +1,20 @@
+﻿
+/*
+2. 
+- For the department with the name: Research and Development Department, which projects are managed 
+by this department currently?
+- Required information: project number, project name, name of management department 
+*/
+
+USE FUH_COMPANY
+GO
+
+SELECT *
+FROM tblDepartment
+
+SELECT *
+FROM tblProject
+
+SELECT proNum, proName, depName
+FROM tblDepartment d JOIN tblProject p ON d.depNum = p.depNum
+WHERE depName = N'Phòng nghiên cứu và phát triển'
