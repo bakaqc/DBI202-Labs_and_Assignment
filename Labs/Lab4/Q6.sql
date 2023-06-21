274b300593a05f75107594cd0f46266293412620
Minh Vương <hardingadonis@gmail.com>
2023-06-21 13:11:14 +0700

    Update ``Q5`` and ``Q6``

--- Labs/Lab4/Q6.sql
+++ Labs/Lab4/Q6.sql
@@ -7,15 +7,10 @@
 USE FUH_COMPANY
 GO
 
--- Step 1: Declare a variable.
-Declare @LocNum VARCHAR(50);
-
--- Step 2: Find locNum of staff named Mai Duy An and assign to @LocNum.
-SELECT @LocNum = locNum
-FROM tblProject
-WHERE proName = N'ProjectA'
-
--- Step 3: Find locNum and locName by @LocNum.
 SELECT locNum AS 'Code', locName AS 'Working position'
 FROM tblLocation
-WHERE locNum = @LocNum;
+WHERE locNum = (
+	SELECT locNum
+	FROM tblProject
+	WHERE proName = N'ProjectA'
+);
