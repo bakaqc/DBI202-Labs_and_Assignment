95ce499c45229bc0479eef683b4fc5714f53661a
Minh Vương <hardingadonis@gmail.com>
2023-06-27 08:37:24 +0700

    Apply template for creating tables in ``Setup.sql``

--- Assignment/SQL/Setup.sql
+++ Assignment/SQL/Setup.sql
@@ -36,16 +36,55 @@ END
 
 
 -- STEP 4: Create Employee table
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Employee')
+BEGIN
+    PRINT 'Employee table created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'The Employee table already exists.';
+END
 
 
 -- STEP 5: Create Product table
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Product')
+BEGIN
+    PRINT 'Product table created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'The Product table already exists.';
+END
 
 
 -- STEP 6: Create Voucher table
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Voucher')
+BEGIN
+    PRINT 'Voucher table created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'The Voucher table already exists.';
+END
 
 
 -- STEP 7: Create Bill table
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Bill')
+BEGIN
+    PRINT 'Bill table created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'The Bill table already exists.';
+END
 
 
 -- STEP 8: Create Bill_Data table
-
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Bill_Data')
+BEGIN
+    PRINT 'Bill_Data table created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'The Bill_Data table already exists.';
+END
