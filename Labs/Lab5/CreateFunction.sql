717ba4448800e67550a0a7ba3a74b0ace493c884
Minh Vương <hardingadonis@gmail.com>
2023-07-08 16:02:51 +0700

    Update

--- Labs/Lab5/CreateFunction.sql
+++ Labs/Lab5/CreateFunction.sql
@@ -1,4 +1,5 @@
-
+﻿-- 1. Write function name: StudenID_ Func1 with parameter @mavt, return the sum
+-- of sl*giaban corresponding
 
 USE ABCCompany
 GO
@@ -17,4 +18,12 @@ BEGIN
 END
 GO
 
+SELECT MaHD, (SL * GiaBan) AS [Số tiền mua VT01 trong mỗi HD]
+FROM CHITIETHOADON
+WHERE MaVT = 'VT01'
+
+SELECT dbo.Group1('VT01') AS SUMGIABAN
 SELECT dbo.Group1('VT02') AS SUMGIABAN
+SELECT dbo.Group1('VT03') AS SUMGIABAN
+SELECT dbo.Group1('VT04') AS SUMGIABAN
+SELECT dbo.Group1('VT05') AS SUMGIABAN
