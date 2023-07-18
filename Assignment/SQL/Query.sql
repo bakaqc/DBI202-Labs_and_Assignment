605347f29e89ad132e84e9d29cc34eaf83ab0b9c
ThundeR <htn10a2@gmail.com>
2023-07-18 15:22:50 +0700

    Update question 1 amd 4 in Query.sql

--- Assignment/SQL/Query.sql
+++ Assignment/SQL/Query.sql
@@ -11,6 +11,13 @@ GO
 -- - Tìm nhân viên tạo hóa đơn nhiều nhất
 -- - Nghĩa
 
+SELECT *
+FROM Employee
+WHERE Employee_ID = ANY (SELECT TOP 1 WITH TIES Employee_ID
+						 FROM Bill
+						 GROUP BY Employee_ID
+						 ORDER BY COUNT(Bill_ID) DESC)
+
 -- Câu 2:
 -- - Hiển thị lịch sử mua hàng của khách hàng thành viên có SĐT là 0956789012
 -- - Chương
@@ -22,6 +29,12 @@ GO
 -- Câu 4:
 -- - Hiển thị danh sách hóa đơn của nhân viên Nguyễn Thị Thúy
 -- - Nghĩa
+
+SELECT *
+FROM Bill
+WHERE Employee_ID = (SELECT Employee_ID
+					 FROM Employee
+					 WHERE Employee_Name = N'Nguyễn Thị Thúy')
 
 -- Câu 5:
 -- - Hiển thị tổng doanh thu của mỗi tháng
