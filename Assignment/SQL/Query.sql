c8afee5ecc1f63cd0f9d7dd3419deb84f8c60bd8
yuhtnguyen <nguyenthithuy1022003@gmail.com>
2023-07-18 13:25:35 +0700

    Add 2 query

--- Assignment/SQL/Query.sql
+++ Assignment/SQL/Query.sql
@@ -31,9 +31,43 @@ GO
 -- - Hiển thị sản phẩm best seller của tháng 7
 -- - Thúy
 
+/*	SELECT p.Product_Name, SUM(bd.Product_Amount) AS Total_sold
+	FROM Bill_Data AS bd
+		JOIN Product p ON bd.Product_ID = p.Product_ID
+		JOIN Bill b ON bd.Bill_ID = b.Bill_ID
+	WHERE MONTH(b.Create_Date) = 7
+	GROUP BY p.Product_Name
+	*/
+
+	SELECT TOP 1 p.Product_Name, SUM(bd.Product_Amount) AS Total_sold
+	FROM Bill_Data AS bd
+		JOIN Product p ON bd.Product_ID = p.Product_ID
+		JOIN Bill b ON bd.Bill_ID = b.Bill_ID
+	WHERE MONTH(b.Create_Date) = 7
+	GROUP BY p.Product_Name
+	ORDER BY Total_Sold DESC;
+
 -- Câu 7:
 -- - Hiển thị khách hàng thành viên có hóa đơn mua lớn nhất trong tháng 6 và thông tin hóa đơn đó
 -- - Thúy
+
+/*	SELECT		 c.Customer_Name, 
+				 b.Bill_ID, b.Create_Date, b. Create_Time, b.Payment_Date, b.Payment_Time, 
+				 b.Primary_Price, b.Used_Point, b.Final_Price, b.Earned_Point, b.Given_Money,
+				 b.Excess_Money, b.Employee_ID, b.Voucher_ID, b.Customer_Phone
+	FROM Customer c
+		JOIN Bill b ON c.Customer_Phone = b.Customer_Phone
+	WHERE MONTH(b.Create_Date) = 6
+	*/
+
+	SELECT TOP 1 c.Customer_Name, 
+				 b.Bill_ID, b.Create_Date, b. Create_Time, b.Payment_Date, b.Payment_Time, 
+				 b.Primary_Price, b.Used_Point, b.Final_Price, b.Earned_Point, b.Given_Money,
+				 b.Excess_Money, b.Employee_ID, b.Voucher_ID, b.Customer_Phone
+	FROM Customer c
+		JOIN Bill b ON c.Customer_Phone = b.Customer_Phone
+	WHERE MONTH(b.Create_Date) = 6
+	ORDER BY b.Primary_Price DESC;
 
 -- Câu 8:
 -- - Hiển thị thông tin sản phẩm có số lượng mua nhiều nhất của các khách hàng vãng lai
