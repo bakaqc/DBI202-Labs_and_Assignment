8a599f6de5fd1cf2f054cc662d4813dff588760f
Minh Vương <hardingadonis@gmail.com>
2023-07-18 12:38:41 +0700

    Add query and assign to members

---
+++ Assignment/SQL/Query.sql
@@ -0,0 +1,40 @@
+--------------------------------------------------------
+--                                                    --
+--                    Write query                     --
+--                                                    --
+--------------------------------------------------------
+
+USE PASTRY_SHOP
+GO
+
+-- Câu 1:
+-- - Tìm nhân viên tạo hóa đơn nhiều nhất
+-- - Nghĩa
+
+-- Câu 2:
+-- - Hiển thị lịch sử mua hàng của khách hàng thành viên có SĐT là 0956789012
+-- - Chương
+
+-- Câu 3:
+-- - Hiển thị số lần được áp dụng của các voucher
+-- - Vương
+
+-- Câu 4:
+-- - Hiển thị danh sách hóa đơn của nhân viên Nguyễn Thị Thúy
+-- - Nghĩa
+
+-- Câu 5:
+-- - Hiển thị tổng doanh thu của mỗi tháng
+-- - Vương
+
+-- Câu 6:
+-- - Hiển thị sản phẩm best seller của tháng 7
+-- - Thúy
+
+-- Câu 7:
+-- - Hiển thị khách hàng thành viên có hóa đơn mua lớn nhất trong tháng 6 và thông tin hóa đơn đó
+-- - Thúy
+
+-- Câu 8:
+-- - Hiển thị thông tin sản phẩm có số lượng mua nhiều nhất của các khách hàng vãng lai
+-- - Chương
